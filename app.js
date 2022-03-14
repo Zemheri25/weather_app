8a918264c4d0fa9f71dac09425a34b283f5e8dc1
Zemheri25 <omerzemheri14@gmail.com>
2022-03-14 15:10:01 +0300

    api_fetch_weather app added

--- app.js
+++ app.js
@@ -4,28 +4,37 @@ let inputbox = document.getElementById("inputbox");
 let cities = document.querySelector(".cities");
 
 async function getApi(url) {
-    let state1 = await fetch(url);
-    let state2 = await state1.json();
-    return state2
+  let state1 = await fetch(url);
+  let state2 = await state1.json();
+  return state2;
 }
 
 // getApi("https://api.openweathermap.org/data/2.5/weather?q=ANKARA&appid=d05a40a07d0a11705e0cd93dc33af6d5&lang=tr&units=metric").then(item => console.log(item.main.temp))
 
-
-
-
 submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    getApi(`https://api.openweathermap.org/data/2.5/weather?q=${inputbox.value[0].toUpperCase() + inputbox.value.substring(1)}&appid=d05a40a07d0a11705e0cd93dc33af6d5&lang=tr&units=metric`).then(item => 
-    cities.innerHTML += `
-    <li>
-          <div class="card">
-            <h2> ${item.name} </h2>
-            <h1>${item.main.temp} C</h1>
-            <h3> ${item.weather[0].description} </h3>
-          </div>
-        </li>
-    
-    `)
-    
-})
+  e.preventDefault();
+  try {
+    getApi(
+      `https://api.openweathermap.org/data/2.5/weather?q=${
+        inputbox.value[0].toUpperCase() + inputbox.value.substring(1)
+      }&appid=d05a40a07d0a11705e0cd93dc33af6d5&lang=tr&units=metric`
+    ).then(
+      (item) =>
+      
+        {console.log(item)
+          cities.innerHTML += `
+      <li>
+            <div class="card">
+              <h2> ${item.name} </h2>
+              <h1>${item.main.temp} C</h1>
+              <img src="http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png" alt="">
+              <h3> ${item.weather[0].description} </h3>
+            </div>
+          </li>
+      
+      `}
+    );
+  } catch (error) {
+    alert(error);
+  }
+});
